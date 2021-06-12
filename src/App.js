76e8c1bfb67464fa888bfd7f9355eb03ab720835
yait-ucess <bandai28@ezweb.ne.jp>
2021-06-12 13:22:26 +0900

    TaskListコンポーネント作成

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './App.module.css';
 import { FaSignInAlt } from 'react-icons/fa';
+import TaskList from './features/task/TaskList';
 
 function App() {
   const Logout = () => {
@@ -13,10 +14,9 @@ function App() {
         <button onClick={Logout} className={styles.signBtn}>
           <FaSignInAlt />
         </button>
+        <TaskList />
       </div>
-      <div className={styles.appDetails}>
-
-      </div>
+      <div className={styles.appDetails}></div>
     </div>
   );
 }
